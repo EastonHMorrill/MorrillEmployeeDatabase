e77aa13ff24a972fb6da534c4fa45152b260783c
EastonHMorrill <emorrill@gus.pittstate.edu>
2021-02-08 15:29:26 -0600

    The code seems to be working, but is not outputting correctly to the text file. Not sure how to fix.

--- MorrillEmployeeDatabase.py
+++ MorrillEmployeeDatabase.py
@@ -1,5 +1,3 @@
-#MorrillEmployeeDatabase
-
 import Person
 
 MorePeople = True
@@ -14,16 +12,10 @@ for x in Filelines:
 
 File.close()
 
-#Ask for employee information
-
-#Create a plain text file that has all of the relevant employee information
-
-#Create a menu that will present the user options aboutwhat they may want to do 
-
 menu = {}
 menu['1']= "- Add Employee"
 menu['2']= "- Delete Employee"
-menu['3']= "- Find Employee"
+menu['3']= "- Edit Employee Information"
 menu['4']= "- Exit Menu"
 
 Bool = True
@@ -33,28 +25,74 @@ while Bool:
     for entry in options:
         print(entry, menu[entry])
     
-    selection=input("Please Select 1-4:")
+    selection=input("Please Select 1-4: ")
     if selection =='1':
-        print("     Add")
+        print("\n Add: ")         ##\n should end the line
         NewEmployee = Person.Person()
         NewEmployee.getPerson()
-        #Need to add to the text file
 
     elif selection =='2':
-        print("     Delete")
-        #Who would you like to delete? (Lastname)
-        if input == Person.lastname():
-            delete Person.row[]
-        def Deleteline():
-
+        print("\n Delete: ")
+        print("Who would you like to remove? (First and Last name): ")      
+        firstname, lastname = input().split(" ")
+        for emp in People:
+            if(emp.firstname == firstname and emp.lastname == lastname):
+               People.remove(emp)
 
     elif selection =='3':
-        print("     Edit")
-        #Who's account would you like to edit? (Lastname)
+        print("\n Edit: ")
+        print("Who would you like to edit? (First and Last name): ")      
+        firstname, lastname = input().split(" ")
+        for emp in People:
+            if(emp.firstname == firstname and emp.lastname == lastname):
+               People.append(emp)
+
+        newmenu={}
+        newmenu['1']= "- Edit Firstname"
+        newmenu['2']= "- Edit Lastname"
+        newmenu['3']= "- Edit Wage"
+        newmenu['4']= "- Edit Hours"
+        newmenu['5']= "- Finish Editing"
+
+        NewBool = True
+
+        while NewBool:
+            options=menu.keys()
+            for entry in options:
+             print(entry, newmenu[entry])
+    
+            selection=input("Please Select 1-5: ")
+
+            if selection == '1':
+                firstname = input("\n Enter New Firstname: ")
+                People.append(firstname)
+            
+            if selection == '2':
+                lastname = input("\n Enter New Lastname: ")
+                People.append(lastname)
+            
+            if selection == '3':
+                wage = input("\n Enter New Wage: ")
+                People.append(wage)
+
+            if selection == '4':
+                hours = input("\n Enter New Hours: ")
+                people.append(Hours)
+
+            if selection == '5':
+                print("\n Finished Editing: ")
+
 
     elif selection =='4':
-        print("     Exit")
+        print("\n Exit: ")
+        File = open('File.txt', 'w')
+        for x in range(len(People)):
+            print(People[x])
+        for emp in People:
+            File.write(str(emp.firstname)+" "+str(emp.lastname)+" "+str(emp.wage)+" "+str(emp.hours)+"\n")
+        File.close()
         Bool = False
+        
 
 
     else:
